e3a1bae03b46a2e7cbd0a48121dc5de2090f6639
nicolaschianella <chianella.nicolas.3c@gmail.com>
2024-02-11 17:46:56 +0100

    Path fix

--- rsync/rsync_prod.sh
+++ rsync/rsync_prod.sh
@@ -13,7 +13,7 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 if [[ "$branch" =~ (main)$ ]]; then
     rsync -e "ssh" --exclude=".idea/" --exclude='.git/' --exclude="__pycache__/" \
     --exclude="/venv" --exclude="*.csv" --exclude="*.log" --exclude=".gitignore"\
-    -rav . guys@guysmachine:/home/guys/streamlit_sales/guysvintedbot_prod
+    -rav . guys@guysmachine:/home/guys/guysvintedbot/guysvintedbot_prod
 else
     echo "Branch is not main. Skipping rsync command."
 fi
