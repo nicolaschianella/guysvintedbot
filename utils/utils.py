17ec8bb947bd332d65808925d11a309406569d53
nicolaschianella <chianella.nicolas.3c@gmail.com>
2024-02-12 21:42:44 +0100

    Refactoring

--- utils/utils.py
+++ utils/utils.py
@@ -10,19 +10,24 @@
 import logging
 
 
-def reformat_clothes_states(clothes_states):
+def reformat_list_strings(list_strings: list, format_type: str="clothes_states") -> str:
     """
-    Reformats clothes states from ['1', '2', ...] to '1,2,...'
-    :param clothes_states: list of clothes states
-    :return: list of reformatted clothes states
+    Reformats a generic list of strings from ['1', '2', ...] to '1,2,...'
+    Args:
+        list_strings: list of str, list to reformat
+        format_type: str, formatted in logs
+
+    Returns: str, concatenated values
+
     """
-    logging.info(f"Received clothes_states: {clothes_states}")
+
+    logging.info(f"Received {format_type}: {list_strings}")
     concat_values = ""
-    for state in clothes_states:
+    for state in list_strings:
         concat_values += state
         concat_values += ","
     concat_values = concat_values[:-1]
 
-    logging.info(f"Successfully formatted clothes_states to {concat_values}")
+    logging.info(f"Successfully formatted {format_type} to {concat_values}")
 
     return concat_values
