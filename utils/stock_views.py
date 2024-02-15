8ab8e75807f7fdea45b251692c78ab0f2f15548c
nicolaschianella <chianella.nicolas.3c@gmail.com>
2024-02-15 13:24:53 +0100

    Added modal to delete clothes in stock

--- utils/stock_views.py
+++ utils/stock_views.py
@@ -67,3 +67,55 @@ class SellClotheView(discord.ui.Modal,
                                           7,
                                           e,
                                           interaction)
+
+
+class DeleteClotheView(discord.ui.Modal,
+                     title="⚠️ Supprimer ce vêtement ? ⚠️"):
+    """
+    Represents the form sent to the user to confirm item deletion
+    """
+
+    # Date of sale
+    deletion_confirmation = discord.ui.TextInput(
+        label="Confirmation",
+        placeholder="Tapez 'oui' pour confirmer"
+    )
+
+    # Called when the form is sent
+    async def on_submit(self, interaction: discord.Interaction) -> None:
+        """
+        Called when the user submits the form. Registers sale in DB
+        Args:
+            interaction: discord.Interaction
+
+        Returns: None
+
+        """
+        try:
+            await interaction.response.defer()
+
+        except Exception as e:
+            await notify_something_went_wrong("DeleteClotheView",
+                                              "on_submit",
+                                              8,
+                                              e,
+                                              interaction)
+
+    # Called if error of any kind
+    async def on_error(self,
+                       interaction: discord.Interaction,
+                       e: Exception) -> None:
+        """
+        Called when something when wrong in the form. Notifies the user.
+        Args:
+            interaction: discord.Interaction
+            e: Exception
+
+        Returns: None
+
+        """
+        await notify_something_went_wrong("DeleteClotheView",
+                                          "on_error",
+                                          9,
+                                          e,
+                                          interaction)
